fe341b5e599ddcf2411a53ed2fe2ffd593157a8c
civalwar114 <49685116+civalwar114@users.noreply.github.com>
2022-05-25 10:42:26 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+const SaramTitle = () => {
+  return (
+    <>
+      <h1>길동이의 율도국 풀코스</h1>
+      <h2>폐업했습니다</h2>
+    </>
+  );
+};
+
+const Saram = (props) => {
+  return (
+    <>
+      <h3>
+        {props.wow}: {props.item}
+      </h3>
+    </>
+  );
+};
+
+const SaramItems = ["망해요", "안해요", "안팔아요", "안사요"];
+
+export default function App() {
+  return (
+    <div className="App">
+      <SaramTitle />
+      {SaramItems.map((item) => {
+        return <Saram wow="wow" item={item} />;
+      })}
+    </div>
+  );
+}
